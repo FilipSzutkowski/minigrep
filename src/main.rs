0efaa5f321ef69092de45e62d1d66f41e5f7fe31
FilipSzutkowski <filip99.ski@gmail.com>
2024-03-11 21:08:25 +0100

    error handling

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
-use std::{env::args, fs::read_to_string};
+use std::{env::args, fs::read_to_string, process};
 
 fn main() {
     let args: Vec<String> = args().collect();
     let Config {
         query: _query,
         file_path,
-    } = Config::new(&args);
+    } = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem with parsing: {err}");
+        process::exit(1)
+    });
 
-    let contents = read_to_string(file_path).expect("Should have been able to read the file");
+    let contents = read_to_string(file_path).unwrap_or_else(|err| {
+        println!("Problem with reading file: {err}");
+        process::exit(1)
+    });
 
     println!("With text:\n{contents}");
 }
@@ -18,13 +24,16 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
-        let query = args.get(1).expect("Specify the search query.").clone();
-        let file_path = args.get(2).expect("Specify the file path.").clone();
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
 
         println!("Searching for '{query}'");
         println!("In file '{file_path}'");
 
-        Config { query, file_path }
+        Ok(Config { query, file_path })
     }
 }

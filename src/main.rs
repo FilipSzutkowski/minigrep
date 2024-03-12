829b4ac72864f833c4c13defa6aaf1bc98c579c7
FilipSzutkowski <filip99.ski@gmail.com>
2024-03-12 22:47:32 +0100

    delete comments

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,6 @@ fn main() {
         process::exit(1)
     });
 
-    // println!("Searching for: '{}'", config.query);
-    // println!("In file: '{}'", config.file_path);
-
     if let Err(e) = run(config) {
         println!("Application error: {e}");
         process::exit(1)

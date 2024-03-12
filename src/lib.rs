23fd411900406256112a6fb42bc242d91313b6cc
FilipSzutkowski <filip99.ski@gmail.com>
2024-03-12 21:34:02 +0100

    Use search

--- src/lib.rs
+++ src/lib.rs
@@ -20,7 +20,9 @@ impl Config {
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = read_to_string(config.file_path)?;
 
-    println!("With text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
 
     Ok(())
 }

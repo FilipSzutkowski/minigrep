0f99ff60bf8d18acd66aa67dc3b2ebd0092cd939
FilipSzutkowski <filip99.ski@gmail.com>
2024-03-12 22:38:36 +0100

    Search case insensitive

--- src/lib.rs
+++ src/lib.rs
@@ -35,18 +35,49 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
         .collect()
 }
 
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let lowercase_query = query.to_lowercase();
+    let mut result = Vec::new();
+
+    for line in contents
+        .lines()
+        .map(|line| line.trim())
+        .filter(|line| line.to_lowercase().contains(&lowercase_query))
+    {
+        result.push(line);
+    }
+
+    result
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn one_result() {
+    fn case_sensitive() {
         let query = "duct";
         let contents = "\
           Rust:
           safe, fast, productive.
-          Pick three.";
+          Pick three.
+          Duct tape.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+          Rust:
+          safe, fast, productive.
+          Pick three.
+          Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        )
     }
 }

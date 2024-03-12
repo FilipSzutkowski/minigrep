43ba0c7eec24b3a673b462f23df5228a58fc345d
FilipSzutkowski <filip99.ski@gmail.com>
2024-03-12 08:40:53 +0100

    init search function and test

---
+++ src/lib.rs
@@ -0,0 +1,50 @@
+use std::{error::Error, fs::read_to_string};
+
+pub struct Config {
+    pub query: String,
+    pub file_path: String,
+}
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Not enough arguments");
+        }
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+
+        Ok(Config { query, file_path })
+    }
+}
+
+pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = read_to_string(config.file_path)?;
+
+    println!("With text:\n{contents}");
+
+    Ok(())
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .map(|line| line.trim())
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+          Rust:
+          safe, fast, productive.
+          Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::{env::args, error::Error, fs::read_to_string, process};
+use minigrep::{run, Config};
+use std::{env::args, process};
 
 fn main() {
     let args: Vec<String> = args().collect();
@@ -13,30 +14,5 @@ fn main() {
     if let Err(e) = run(config) {
         println!("Application error: {e}");
         process::exit(1)
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-
-        Ok(Config { query, file_path })
     }
 }

eb4b6ba863ad8744cf72d7eee22d2451afe3a49b
FilipSzutkowski <filip99.ski@gmail.com>
2024-03-15 13:32:28 +0100

    Write errors to std err

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,12 @@ use std::{env::args, process};
 fn main() {
     let args: Vec<String> = args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem with parsing: {err}");
+        eprintln!("Problem with parsing: {err}");
         process::exit(1)
     });
 
     if let Err(e) = run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1)
     }
 }
